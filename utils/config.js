55ddd501e1b723b8f275306aa7f99686ed70bba2
pdx_jie <pdx_jie@163.com>
2023-12-20 00:38:56 +0800

    feat: commit

---
+++ utils/config.js
@@ -0,0 +1,27 @@
+var DOMAIN = "http://localhost:8080/";
+var WEBSITENAME="编程灵感库";
+var PAGECOUNT='10';
+var WECHAT='微信号：pdx_jie';
+
+const enableScanLogin =false 
+const enableChannels =false 
+
+const uploadImageSize=1
+
+//小程序原始id
+const appghId ='gh_e49213784fae'
+
+const minapperVersion=4.681
+const minapperSource="free"
+
+export default {
+  getDomain: DOMAIN,
+  getWebsiteName: WEBSITENAME,  
+  getPageCount: PAGECOUNT,
+  getWecat: WECHAT,
+  enableScanLogin,
+  minapperVersion,
+  minapperSource,
+  enableChannels,
+  appghId
+}
